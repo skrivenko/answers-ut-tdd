e45815eb97e4b8f8c731a1bc77943fd8ae673d9e
skrivenko <72100114+skrivenko@users.noreply.github.com>
2026-07-04 16:21:53 +0300

    Шаг 2. Тест 1

--- practice/day1/string_calculator/tests/test_string_calculator.py
+++ practice/day1/string_calculator/tests/test_string_calculator.py
@@ -13,3 +13,6 @@ def test_add_single_number_returns_same_number():
 def test_add_two_numbers_returns_sum():
     assert add("1.1,2.2") == "3.3"
 
+
+def test_add_newline_as_separator():
+    assert add("1\n2,3") == "6"
