20484c5418224220814be7f85f60878c74273639
skrivenko <72100114+skrivenko@users.noreply.github.com>
2026-07-04 16:17:16 +0300

    Шаг 1

--- practice/day1/string_calculator/tests/test_string_calculator.py
+++ practice/day1/string_calculator/tests/test_string_calculator.py
@@ -1,1 +1,15 @@
+import pytest
+from string_calculator import add
+
+
+def test_add_empty_string_returns_zero():
+    assert add("") == "0"
+
+
+def test_add_single_number_returns_same_number():
+    assert add("1") == "1"
+
+
+def test_add_two_numbers_returns_sum():
+    assert add("1.1,2.2") == "3.3"
 
