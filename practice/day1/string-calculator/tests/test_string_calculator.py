6c0f045ed7e82c34b4ef0299fbdf38bf52519ad2
Svetlana Krivenko <svkrivenko@yandex.ru>
2026-07-04 23:30:29 +0300

    Step 3: two numbers

--- practice/day1/string-calculator/tests/test_string_calculator.py
+++ practice/day1/string-calculator/tests/test_string_calculator.py
@@ -2,12 +2,20 @@ import pytest
 
 from app.string_calculator import *
 
+
 def test_add_empty_string_returns_zero():
     calculator = StringCalculator()
     result = calculator.add("")
     assert result == "0"
 
+
 def test_add_single_number_returns_same_number():
     calculator = StringCalculator()
     result = calculator.add("1")
     assert result == "1"
+
+
+def test_add_two_numbers_returns_sum():
+    calculator = StringCalculator()
+    result = calculator.add("1.1,2.2")
+    assert result== "3.3"
