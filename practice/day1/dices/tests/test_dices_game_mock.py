aee50fe508fe03d3af8e36850e097fd6bba3f769
skrivenko <72100114+skrivenko@users.noreply.github.com>
2026-07-04 15:18:38 +0300

    Update test cases for player win/lose scenarios
    Refactor test cases to verify player outcomes based on scores.

--- practice/day1/dices/tests/test_dices_game_mock.py
+++ practice/day1/dices/tests/test_dices_game_mock.py
@@ -3,21 +3,26 @@ from mockito import mock, times, verify, when, ANY
 from app import *
 
 
-# test for behavior 
-def test_game_calls_win_for_all_winners_and_loosers_loose(monkeypatch):
+def test_player_looses_when_he_played_non_winning_score(monkeypatch):
     game = RollDiceGame()
     when(Dice).roll().thenReturn(5)
-    winner_1 = mocked_player_with_bet(game, 5)
-    winner_2 = mocked_player_with_bet(game, 5)
-    looser_1 = mocked_player_with_bet(game, 1)
-    looser_2 = mocked_player_with_bet(game, 2)
+    player = mocked_player_with_bet(game, 1)
 
     game.play()
 
-    verify(winner_1, times(1)).win(Chip(18))
-    verify(winner_2, times(1)).win(Chip(18))
-    verify(looser_1, times(0)).win(ANY)
-    verify(looser_2, times(0)).win(ANY)
+    verify(player, times(1)).take(Chip(3))
+    verify(player, times(0)).win(ANY)
+
+
+def test_player_wins_when_he_played_winning_score(monkeypatch):
+    game = RollDiceGame()
+    when(Dice).roll().thenReturn(5)
+    player = mocked_player_with_bet(game, 5)
+
+    game.play()
+
+    verify(player, times(1)).take(Chip(3))
+    verify(player, times(1)).win(Chip(18))
 
 
 ### private functions to set up test data
